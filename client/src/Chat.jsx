22d4cb01c8dfa2ed887b922b6484fb0c51006ba6
RenanHMF <renanhmf2@gmail.com>
2022-08-19 16:04:51 -0300

    feat: Adicionado balão de identificaão do usuário que enviou a mensagem.

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -33,6 +33,21 @@ const Messages = ({ user }) => {
           paddingBottom : "1em",
         }}
       >
+        {user !== messageUser && (
+          <div
+            style={{
+              height : 50,
+              width : 50,
+              marginRight : '0.5em',
+              border: '2px solid #e5e6ea',
+              borderRadius : 25,
+              textAlign : "center",
+              fontSize : "18pt",
+            }}
+          >
+            {messageUser.slice(0, 2).toUpperCase()}
+          </div>
+        )}
         <div
           style = {{
             background : user === messageUser ? "#58bf56" : "#e5e6ea",
